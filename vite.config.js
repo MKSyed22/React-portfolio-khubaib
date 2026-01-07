e23bec98cf052671ebba6633127b3bb5c667f250
MKSyed22 <mkscusit12093@gmail.com>
2026-01-07 09:50:29 +0300

    Build for GitHub Pages in docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from '@svgr/rollup';
+import svgr from '@svgr/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-portfolio-khubaib/',
+  base: '/React-portfolio-khubaib/',   // your GitHub repo name
   plugins: [react(), svgr()],
   server: {
     port: 3000,
-  }
+  },
+  build: {
+    outDir: 'docs',                     // <-- add this line
+  },
 })
+
