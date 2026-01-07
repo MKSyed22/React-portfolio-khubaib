6a609d5c37704791c854942db02f76c11367e97c
MKSyed22 <mkscusit12093@gmail.com>
2026-01-07 09:39:57 +0300

    Fix GitHub Pages bade path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/React-portfolio-khubaib/',
   plugins: [react(), svgr()],
   server: {
     port: 3000,
